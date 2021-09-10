079e5dea65901dac367fbe1c91d743dc7b9fa0d6
Paul <chorkunxin@gmail.com>
2021-09-10 15:33:25 +0800

    Added C and R

--- lab07-crud/index.js
+++ lab07-crud/index.js
@@ -16,12 +16,42 @@ app.use(express.urlencoded({
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts');
 
+const API_BASE_URL = "https://petstore.swagger.io/v2/"
+
 // routes
 app.get('/', async function(req,res){
    let response = await axios.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available");
    res.render('pets',{
        'allPets': response.data
    })
+})
+
+// display the create pet form
+app.get('/pet/create', function(req,res){
+    res.render('create_pet')
+})
+
+app.post('/pet/create', async function(req,res){
+ let data = {
+    "id": Math.floor(Math.random() * 99999 + 10000),
+    "category": {
+      "id": Math.floor(Math.random() * 99999 + 10000),
+      "name": req.body.category
+    },
+    "name": req.body.petName,
+    "photoUrls": [
+      "string"
+    ],
+    "tags": [
+      {
+        "id": 0,
+        "name": "string"
+      }
+    ],
+    "status": "available"
+  }
+  await axios.post(API_BASE_URL+"pet/", data);
+  res.send("Adding pet successful");
 })
 
 app.listen(3000, function(){
