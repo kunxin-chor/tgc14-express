d8f148850323110bc0de58911c6c251c9b988a17
Paul <chorkunxin@gmail.com>
2021-09-10 15:57:27 +0800

    Added validation

--- lab07-crud/index.js
+++ lab07-crud/index.js
@@ -19,41 +19,66 @@ wax.setLayoutPath('./views/layouts');
 const API_BASE_URL = "https://petstore.swagger.io/v2/"
 
 // routes
-app.get('/', async function(req,res){
-   let response = await axios.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available");
-   res.render('pets',{
-       'allPets': response.data
-   })
+app.get('/', async function (req, res) {
+    let response = await axios.get("https://petstore.swagger.io/v2/pet/findByStatus?status=available");
+    res.render('pets', {
+        'allPets': response.data
+    })
 })
 
 // display the create pet form
-app.get('/pet/create', function(req,res){
+app.get('/pet/create', function (req, res) {
     res.render('create_pet')
 })
 
-app.post('/pet/create', async function(req,res){
- let data = {
-    "id": Math.floor(Math.random() * 99999 + 10000),
-    "category": {
-      "id": Math.floor(Math.random() * 99999 + 10000),
-      "name": req.body.category
-    },
-    "name": req.body.petName,
-    "photoUrls": [
-      "string"
-    ],
-    "tags": [
-      {
-        "id": 0,
-        "name": "string"
-      }
-    ],
-    "status": "available"
-  }
-  await axios.post(API_BASE_URL+"pet/", data);
-  res.send("Adding pet successful");
+app.post('/pet/create', async function (req, res) {
+
+    // basic validation
+    // length of petName > 0
+    // length of category > 0
+
+    let errors = [];
+    if (req.body.petName.length == 0) {
+        errors.push("Please provide a name for the pet")
+    }
+
+    if (req.body.category.length == 0) {
+        errors.push("Please provide a category");
+    }
+
+    // check if no errors
+    if (errors.length == 0) {
+        // if no errors, proceed to create the animal using the API endpoint
+        let data = {
+            "id": Math.floor(Math.random() * 99999 + 10000),
+            "category": {
+                "id": Math.floor(Math.random() * 99999 + 10000),
+                "name": req.body.category
+            },
+            "name": req.body.petName,
+            "photoUrls": [
+                "string"
+            ],
+            "tags": [
+                {
+                    "id": 0,
+                    "name": "string"
+                }
+            ],
+            "status": "available"
+        }
+        await axios.post(API_BASE_URL + "pet/", data);
+        res.redirect('/');
+    } else {
+        // if have errors, then we render the hbs file with the form again
+        // but this time we pass the errors to the hbs
+        res.render('create_pet', {
+            'errors': errors
+        })
+    }
+
 })
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started")
 })
